fe6a9c4ecaedd875f5e88043a0e68b613fed3e8d
ritik0002 <ritik_d@hotmail.com>
2022-12-01 20:32:16 +0000

    typescript and vue

---
+++ auction_project/auction/frontend/src/App.vue
@@ -0,0 +1,22 @@
+
+<template>
+  <div>
+    Hello World
+  </div>
+</template>
+
+<script setup lang="ts">
+// export default{
+//   data(){
+
+//   }
+
+
+
+</script>
+
+
+
+<style scoped>
+
+</style>
