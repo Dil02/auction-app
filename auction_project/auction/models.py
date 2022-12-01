feb8ba1eec9f2f7b3ade6a81c525eb20d0946241
Pravin Prabhakaran <pravin.prabha123248@gmail.com>
2022-12-01 21:27:45 +0000

    Added the models

--- auction_project/auction/models.py
+++ auction_project/auction/models.py
@@ -1,6 +1,72 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+
+#Changes made from the diagram : 
+#   Removed endate from Bid and changed Start to Time (for the exact time the bid was placed)
+#   Haven't done it but maybe change price in Item to startPrice
+#   Admin account is : admin, test
+
+
 class User(AbstractUser):
     pass
-# Create your models here.
+
+class Item(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(max_length=1000)
+    condition = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10,decimal_places=2)
+    start = models.DateField('Start Date')
+    end = models.DateField('End Date')
+    picture = models.CharField(max_length=255)
+    sold = models.BooleanField(default=False)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        #return ("ID : " + str(self.id) + ", Name : " + self.name + ", Condition : " + self.condition + ", Price : " + str(self.price) + ", Start : " + str(self.start) + ", End : " + str(self.end) + ", Sold : " + str(self.sold) + ", Owner : " + str(self.owner))        
+        return ("ID : " + str(self.id) + ", Name : " + self.name)
+    
+class Bid(models.Model):
+    bidder = models.ForeignKey(User, on_delete=models.CASCADE)
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    time = models.DateTimeField('Bid time')
+    amount = models.DecimalField(max_digits=10,decimal_places=2)
+
+
+    def __str__(self):
+        #return ("ID : " + str(self.id) + ", Bidder : " + str(self.bidder) + ", Item : " + str(self.item) + ", Time : " + str(self.time) + ", Amount : " + str(self.amount))
+        return ("ID : " + str(self.id) + ", Bidder : " + str(self.bidder) + ", Item : " + str(self.item))
+    def to_dict(self):
+        """Returns a dictionary of Bid contents"""
+        return {
+            'id': self.id,
+            'bidder': self.bidder,
+            'item': self.item,
+            'start': self.start,
+            'end': self.end,
+            'amount':self.amount,
+        }
+
+
+class Question(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField(max_length=1000)
+    response = models.TextField(max_length=1000,default=None, blank=True, null=True)
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    questioner = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        #return ("ID : " + str(self.id) + ", Title : " + str(self.title) + ", Description : " + str(self.description) + ", Response : " + str(self.response) + ", Item : " + str(self.item) + ", Questioner : " + str(self.questioner))
+        return ("ID : " + str(self.id) + ", Title : " + str(self.title))
+
+    def to_dict(self):
+        """Returns a dictionary of Question contents"""
+        return {
+            'id': self.id,
+            'title': self.title,
+            'description': self.description,
+            'response': self.response,
+            'item': self.item,
+            'questioner':self.questioner,
+        }
+
---
+++ auction_project/auction/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    #path('', views.index, name='index'),
+    
+    #Item API paths
+    #path('api/items/', views.items_api, name="items"),
+    #path('api/items/<int:itemID>/', views.item_api, name="item"),
+
+    #Bid API paths
+    #path('api/bids/', views.bids_api, name="bids"),
+    #path('api/bids/<int:bidID>/', views.bids_api, name="bid"),
+
+    #Question paths
+    #path('api/questions/', views.questions_api, name="questions"),
+    #path('api/questions/<int:questionID>/', views.question_api, name="question"),
+]
