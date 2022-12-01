f0e2bb82e9a814eff1e11dceea24765ef6b737ee
ritik0002 <ritik_d@hotmail.com>
2022-12-01 21:35:35 +0000

    Did the vue router

--- auction_project/auction/frontend/src/App.vue
+++ auction_project/auction/frontend/src/App.vue
@@ -1,15 +1,15 @@
 
 <template>
-  <div>
-    Hello World
-  </div>
+  <!-- Temp nav bar -->
+  <nav>
+    <ul> 
+    <li><router-link to="/home">Home test</router-link> </li>   <!-- DO /test to go to the hello world page or click this!-->
+  </ul>  
+  </nav>
+    <RouterView></RouterView>
 </template>
 
 <script setup lang="ts">
-// export default{
-//   data(){
-
-//   }
 
 
 
