ec717f2b68b1bdd7dfb25bdce5cec812bc96205a
Dil02 <77886292+Dil02@users.noreply.github.com>
2022-12-08 14:05:31 +0000

    basic template

---
+++ auction_project/auction/frontend/src/App.vue
@@ -0,0 +1,22 @@
+
+<template>
+  <!-- Temp nav bar -->
+  <nav>
+    <ul> 
+    <li><router-link to="/home">Home test</router-link> </li>   <!-- DO /test to go to the hello world page or click this!-->
+  </ul>  
+  </nav>
+    <RouterView></RouterView>
+</template>
+
+<script setup lang="ts">
+
+
+
+</script>
+
+
+
+<style scoped>
+
+</style>
