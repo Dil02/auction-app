5bdcbbf5120dc632b45b16aca667c71ba90d2726
ritik0002 <ritik_d@hotmail.com>
2022-12-08 21:25:17 +0000

    Image done

--- auction_project/auction/frontend/src/components/AddItem.vue
+++ auction_project/auction/frontend/src/components/AddItem.vue
@@ -1,27 +1,27 @@
 <template>
     <div>
         <form
-            class="w-75 mx-auto mt-2 pt-2 d-flex flex-column justify-content-center align-items-center  p-2 bg-dark text-light "
-            id="form1" @submit="saveNewObject()">
-            <div class="form-input">
-                <label class="form-label" for="image">Choose a image</label>
-                <input accept="image/jpeg, image/png" type="file" class="form-control" @change="uploadImages" id="image"
-                    required />
+            class="w-75 mx-auto mt-2 pt-2 d-flex flex-column justify-content-center p-2 bg-dark text-light rounded"
+            id="form1" @submit.prevent="saveNewObject()">
+            <div class="form-input ml-auto">
+                <h3><label class="form-label" for="image">Choose a image</label></h3>
+               <input type="file" accept="image/*" @change="storeImage" class="form-control-file">
+                <img class="img-fluid rounded mx-auto" height=400 v-if="picture" :src="picture" />
             </div>
             <div class="form-input">
-                <label class="form-label" for="name">Name of Product</label>
+                <h3><label class="form-label" for="name">Name of Product</label></h3>
                 <input v-model="name" type="text" class="form-control" placeholder="Enter name">
             </div>
-            <div class="form-group mb-2">
-                <label for="Description">Description</label>
+            <div class="form-input mb-2">
+                <h3><label for="Description">Description</label></h3>
                 <textarea v-model="desc" class="form-control" id="Desc" rows="4" required></textarea>
             </div>
             <div class="form-input">
-                <label class="form-label" for="price">Set Price</label>
+                <h3><label class="form-label" for="price">Set Price</label></h3>
                 <input v-model="price" type="text" class="form-control" placeholder="Enter name">
             </div>
             <div class="form-group">
-                <label for="options">Select Condition</label>
+                <h3><label for="options">Select Condition</label></h3>
                 <select v-model="type" class="form-control" id="options" required>
                     <option value="NEW">NEW</option>
                     <option value="UE">USED - EXCELLENT</option>
@@ -32,20 +32,20 @@
             <div class="form-group mb-2 inline">
 
                 <div class="d-inline">
-                    <label for="startDate">Start Date</label>
+                    <h3><label for="startDate">Start Date</label></h3>
                     <div style="clear: both;"></div>
                     <input v-model=start id="startDate" class="form-control" type="date" required />
                 </div>
 
                 <div class="d-inline">
-                    <label for="endDate">End Date</label>
+                    <h3><label for="endDate">End Date</label></h3>
                     <div style="clear: both;"></div>
                     <input v-model=end id="endDate" class="form-control" type="date" required />
                 </div>
 
             </div>
 
-            <button type="submit" class="btn btn-primary mt-4 pt-2 w-50 mb-3">Submit</button>
+            <button type="submit" class="btn btn-primary mt-4 pt-2 w-50 mb-3" id="b">Submit</button>
 
         </form>
     </div>
@@ -57,9 +57,10 @@ export default {
 
     data() {
         return {
+            prev:"",
             type: "",
             name: "",
-            picture: null,
+            picture: '',
             price: null,
             start: null,
             end: null,
@@ -71,31 +72,58 @@ export default {
     methods: {
         async saveNewObject() {
             const item = JSON.stringify({
-                name:this.name,
+                name: this.name,
                 Type: this.type,
                 price: this.price,
                 description: this.desc,
                 start: this.start,
                 end: this.end,
-                picture:this.picture,
+                picture: this.picture,
             })
             console.log(item)
             let response = await fetch("http://localhost:8000/api/items/", {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json',
-                    },
-                    body: item,
-                })
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: item,
+            })
 
         },
-        uploadImages(e: any) {
-            this.picture = e.target.files[0];
+        // storeImage(e:any) {
+        //     var files = e.target.files || e.dataTransfer.files;
+        //     if (!files.length)
+        //         return;
+        //     this.createImage(files[0]);
+        // },
+        // createImage(file:any) {
+        //     var picture = new Image();
+        //     var reader = new FileReader();
+
+        //     reader.onload = (e:any) => {
+        //         this.picture = e.target?.result;
+        //     };
+        //     reader.readAsDataURL(file);
+        // }
+        async storeImage(e:any){
+            const file = e.target.files[0];
+            this.prev = URL.createObjectURL(file);  //creates a blob image (to preview image)
+            var reader = new FileReader();
+            this.picture = await new Response(this.prev).text()  // changes blob to string (to store)
+
         }
     }
 }  
 </script>
 
 <style scoped>
-
+#b{
+    margin:auto auto;
+}
+img{
+    max-height:300px;
+    max-width:300px;
+    height:auto;
+    width:auto;
+}
 </style>
