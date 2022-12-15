a721aacfffed0b2b01ac2e2b81c8d0f1d4b39b47
Pravin Prabhakaran <pravin.prabha123248@gmail.com>
2022-12-15 23:59:11 +0000

    fixed profile

--- auction_project/auction/frontend/src/components/Profile.vue
+++ auction_project/auction/frontend/src/components/Profile.vue
@@ -9,38 +9,37 @@
             <div class="col-sm-6">
                 <div class="form-floating mb-3">
                     <input type="text" class="form-control bg-light" placeholder="nameExample" id="profileUsername"
-                        v-model="userDetails.username">
+                        v-model="username">
                     <label for="profileUsername">Username</label>
                 </div>
                 <div class="form-floating mb-3">
                     <input type="text" class="form-control bg-light" placeholder="Clyde" id="profileFirstName"
-                        v-model="userDetails.fname">
+                        v-model="fname">
                     <label for="profileFirstName">First Name</label>
                 </div>
                 <div class="form-floating mb-3">
                     <input type="text" class="form-control bg-light" placeholder="Tyler" id="profileSurname"
-                        v-model="userDetails.sname">
+                        v-model="sname">
                     <label for="profileSurname">Surname</label>
                 </div>
                 <div class="form-floating mb-3">
                     <input type="text" class="form-control bg-light" placeholder="name@example.com" id="profileEmail"
-                        v-model="userDetails.email">
+                        v-model="email">
                     <label for="profileEmail">Email Address</label>
                 </div>
                 <div class="form-floating mb-3">
                     <input type="text" class="form-control bg-light" placeholder="01/01/2000" id="profileDOB"
-                        v-model="userDetails.dob">
+                        v-model="dob">
                     <label for="profileDOB">Date of Birth</label>
                 </div>
                 <div class="form-floating mb-3">
                     <input type="text" class="form-control bg-light" placeholder="London" id="profileCity"
-                        v-model="userDetails.city">
+                        v-model="city">
                     <label for="profileCity">City</label>
                 </div>
             </div>
             <div class="col-sm-6">
-                <img v-bind:src="'http://localhost:8000' + userDetails.picture" height="350" width="350"
-                    alt="Press 'View Profile'">
+                <img v-bind:src="'http://localhost:8000' + picture" height="350" width="350" alt="Press 'View Profile'">
 
                 <form method="POST" enctype="multipart/form-data" class="mt-2" @submit.prevent="updateUserPicture()">
                     <input type="file" accept="image/*" name="myFile" id="profileInput">
@@ -60,9 +59,10 @@
     </div>
 </template>
 
-<script>
-function getCookie(name) {
-    let cookieValue = null;
+<script lang="ts">
+
+function getCookie(name: String) {
+    let cookieValue = "";
     if (document.cookie && document.cookie !== '') {
         const cookies = document.cookie.split(';');
         for (let i = 0; i < cookies.length; i++) {
@@ -76,11 +76,20 @@ function getCookie(name) {
     }
     return cookieValue;
 }
+
 // Adding local state
 export default {
     data() {
         return {
-            userDetails: [],
+            username: null as null | string,
+            fname: null as null | string,
+            sname: null as null | string,
+            dob: null as null | string,
+            city: null as null | string,
+            email: null as null | string,
+            id: null as null | string,
+            picture: null as null | string
+
         }
     },
     mounted() {
@@ -89,7 +98,15 @@ export default {
     methods: {
         async updateUserDetails() {
             //Performs an Ajax PUT request to update a user's profile.
-            let response = await fetch("http://127.0.0.1:8000/api/users/" + this.userDetails.id + "/", {
+            let givenUser = document.getElementById("profileUsername") as HTMLInputElement
+            let givenFname = document.getElementById("profileFirstName") as HTMLInputElement
+            let givenSname = document.getElementById("profileSurname") as HTMLInputElement
+            let givenDob = document.getElementById("profileDOB") as HTMLInputElement
+            let givenCity = document.getElementById("profileCity") as HTMLInputElement
+            let givenEmail = document.getElementById("profileEmail") as HTMLInputElement
+
+
+            let response = await fetch("http://127.0.0.1:8000/api/users/" + this.id + "/", {
                 method: 'PUT',
                 credentials: "include",
                 mode: "cors",
@@ -100,24 +117,28 @@ export default {
                 },
                 body: JSON.stringify({
                     data: {
-                        username: document.getElementById("profileUsername").value,
-                        fname: document.getElementById("profileFirstName").value,
-                        sname: document.getElementById("profileSurname").value,
-                        dob: new Date(document.getElementById("profileDOB").value).toISOString().split('T')[0],
-                        city: document.getElementById("profileCity").value,
-                        email: document.getElementById("profileEmail").value,
+                        username: givenUser.value,
+                        fname: givenFname.value,
+                        sname: givenSname.value,
+                        dob: new Date(givenDob.value).toISOString().split('T')[0],
+                        city: givenCity.value,
+                        email: givenEmail.value,
                     }
                 })
             })
             let data = await response.json();
-            this.updatedAt = data.updatedAt;
-            this.$router.push('/')
+            this.fetchUserDetails();
         },
+
         async updateUserPicture() {
             const formData = new FormData()
-            let fileField = document.querySelector("#profileInput")
-            formData.append('myFile', fileField.files[0])
-            let response = await fetch("http://127.0.0.1:8000/api/profile/" + this.userDetails.id + "/", {
+            let fileField = document.querySelector("#profileInput") as HTMLInputElement;
+
+            if (fileField.files && fileField.files.length > 0) {
+                formData.append('myFile', fileField.files[0])
+            }
+
+            let response = await fetch("http://127.0.0.1:8000/api/profile/" + this.id + "/", {
                 method: 'POST',
                 credentials: "include",
                 mode: "cors",
@@ -129,15 +150,24 @@ export default {
             })
             let data = await response.json();
         },
+
         async fetchUserDetails() {
             let response = await fetch("http://127.0.0.1:8000/api/sessionUser/", { credentials: "include", mode: "cors", referrerPolicy: "no-referrer" })
             let data = await response.json();
-            if (data.User == "None") {
-                window.location.href = "http://127.0.0.1:8000/login/"
-            }
-            this.userDetails = data.User
+            let record = data.User
+            this.username = record.username;
+            this.fname = record.fname;
+            this.sname = record.sname;
+            this.dob = record.dob;
+            this.city = record.city;
+            this.email = record.email;
+            this.id = record.id;
+            this.picture = record.picture;
+
         },
     }
 }
+
 import 'bootstrap/dist/css/bootstrap.css';
+
 </script>
